b02ecbb0bec8a95a996b6386742afbad0749836c
MarkMastro <mastropaolo.mark@gmail.com>
2022-03-19 12:25:58 -0400

    complete part 2 styling

--- src/components/Student.js
+++ src/components/Student.js
@@ -14,15 +14,19 @@ export default function Student(props){
     } = props;
     const avg = grades.reduce((a, b) => parseInt(a) + parseInt(b)) / grades.length;
     return(
-        <div className="student-profile">
+        <div className="student">
             <img className="student-picture" src={pic} alt="Student"/>
-            <h1 className="name">{firstName} {lastName}</h1> 
-            <div className="student-information">
-                <p >Email: {email}</p>
-                <p>Company: {company}</p>
-                <p>Skill: {skill}</p>
-                <p>Average: {avg} </p>
+            <div className='student-information'>
+                <h1 className="student-name">{firstName} {lastName}</h1> 
+                <div className="student-profile">
+                    <p>Email: {email}</p>
+                    <p>Company: {company}</p>
+                    <p>Skill: {skill}</p>
+                    <p>Average: {avg} </p>
+                </div>
+
             </div>
+           
             
         </div>
 
